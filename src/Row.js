487b062f891d5de5b73a08a53bb8aef8bde342b6
rodriguezje96 <rodriguez.je96@gmail.com>
2021-09-01 20:30:06 -0300

    cambios de estilo
    -Los posters de cada pelicula se agrandan al posicionar el mouse
    -Se elimina la barra de navegacion de cada row

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import "./Row.css";
 
-const baseUrl = 'https://image.tmdb.org/t/p/original'
-
-function Row({ title, fetchUrl }) {
+const baseUrl = 'https://image.tmdb.org/t/p/w185'
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -23,14 +23,14 @@ function Row({ title, fetchUrl }) {
         <div className="row">
             <h2>{title}</h2>
 
-        <div className="row__posters">
-            {movies.map(movie => (
-            <img src={`${baseUrl}${movie.poster_path}`} alt={movie.name} />
+            <div className="row-posters">
+                {movies.map(movie => (
+                    <img key={movie.id} src={`${baseUrl}${movie.poster_path}`} alt={movie.name} />
 
-            ))}
+                ))}
 
 
-        </div>
+            </div>
 
 
         </div>
