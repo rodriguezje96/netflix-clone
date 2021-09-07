5f06187c2a1ba6105745507fd066a9c32ce1c8eb
rodriguezje96 <rodriguez.je96@gmail.com>
2021-09-06 22:54:59 -0300

    banner
    -Estilos del banner
    -Estilos de botones de "ver" y "mi lista"
    -Funcion truncate para limitar caracteres en pantalla

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './Requests';
-
+import './banner.css';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -18,6 +18,10 @@ function Banner() {
     }, []);
     console.log(movie);
 
+    function truncate(str, n)   {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
+
     return (
         <header className="banner"
             style={{
@@ -30,10 +34,16 @@ function Banner() {
         >
 
             <div className="banner-contents">
-                <h1> 
+                <h1 className="banner-title"> 
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
 
+            <div className="banner-buttons">
+            <button className="banner-button">Play</button>
+            <button className="banner-button">My List</button>
+            </div>
+
+            <h1 className="banner-description">{truncate(movie?.overview, 170)}</h1>
 
             </div>
 
